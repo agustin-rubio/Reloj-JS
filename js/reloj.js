381702b9d6131bf9326292e62985785b56d7b033
Agustin_Rubio <rubioagustin24@gmail.com>
2023-04-24 20:49:12 -0300

    Index html + css + javascript

---
+++ js/reloj.js
@@ -0,0 +1,31 @@
+const intl = Intl.DateTimeFormat(
+    'sp-AR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }
+)
+
+setInterval(() => {
+    const hora = intl.format(new Date())
+    const segundos = hora.second
+    document.getElementById("container").innerHTML = `${hora}`
+}, 1000)
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+// }
+
+// console.log(getRandomInt(3));
+
+const cont = document.querySelector(".container")
+const back = document.querySelector(".body")
+const extra = document.querySelector(".extra-info")
+
+function cambiaColor() {
+    cont.style.color = "black";
+    cont.style.backgroundColor = "red";
+    back.style.backgroundColor = "white";
+    extra.style.color = "black";
+    document.getElementById("extra").innerHTML = `YOU TOUCHED IT`
+}
